37114814d216e5ec6cca99a0c41d03da7cb60ab2
Miles Johnson <mileswjohnson@gmail.com>
2023-12-20 12:41:15 -0800

    Update workflow.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,8 @@
+#[cfg(feature = "wasm")]
+mod helpers;
+#[cfg(feature = "wasm")]
+mod proto;
 mod toolchain_toml;
 
-// WASM cannot be executed through the test runner and we need to avoid building
-// WASM code for non-WASM targets. We can solve both of these with a cfg flag.
-
-#[cfg(not(test))]
-mod helpers;
-
-#[cfg(not(test))]
-mod proto;
-
-#[cfg(not(test))]
+#[cfg(feature = "wasm")]
 pub use proto::*;
