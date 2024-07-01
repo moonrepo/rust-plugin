8ea25bbf18369a97cfe97e4d673f21829386d5cf
Miles Johnson <mileswjohnson@gmail.com>
2024-06-30 18:01:52 -0700

    Update home detection.

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,24 +1,34 @@
+use std::path::PathBuf;
+
 use extism_pdk::*;
 use proto_pdk::*;
-use std::path::PathBuf;
 
 #[host_fn]
 extern "ExtismHost" {
     fn get_env_var(name: String) -> String;
+    fn to_virtual_path(input: String) -> String;
 }
 
-pub fn get_rustup_home(env: &HostEnvironment) -> Result<PathBuf, Error> {
+pub fn get_rustup_home(env: &HostEnvironment) -> Result<VirtualPath, Error> {
     // Cargo sets the RUSTUP_HOME env var when running tests,
     // which causes a ton of issues, so intercept it here!
     if let Some(test_env) = get_test_environment()? {
-        return Ok(test_env.sandbox.join(".home/.rustup"));
+        return Ok(virtual_path!(buf, test_env.sandbox).join(".home/.rustup"));
     }
 
-    // Variable returns a real path
-    Ok(host_env!("RUSTUP_HOME")
-        .map(PathBuf::from)
-        // So we need our fallback to also be a real path
-        .unwrap_or_else(|| env.home_dir.real_path().unwrap().join(".rustup")))
+    Ok(match host_env!("RUSTUP_HOME") {
+        Some(path) => {
+            let path = PathBuf::from(path);
+
+            // Variable returns a real path, so convert to virtual
+            if path.is_absolute() {
+                virtual_path!(buf, path)
+            } else {
+                virtual_path!("/cwd").join(path)
+            }
+        }
+        None => env.home_dir.join(".rustup"),
+    })
 }
 
 pub fn get_channel_from_version(spec: &VersionSpec) -> String {
